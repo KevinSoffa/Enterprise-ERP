349c87f47c7ec242a726962a89328b4b00ad8981
[Kevin Soffa] <[kevinsoffa@gmail.com]>
2025-07-11 12:31:21 -0300

    criando mais models para companies

--- companies/models.py
+++ companies/models.py
@@ -19,3 +19,20 @@ class Employee(models.Model):
         Enterprise,
         on_delete=models.CASCADE
     )
+
+class TaskStatus(models.Model):
+    name = models.CharField(max_length=155)
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'companies_task_status'
+
+class Task(models.Model):
+    title = models.TextField()
+    description = models.TextField(null=True)
+    due_date = models.DateTimeField(null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(null=True)
+    status = models.ForeignKey(TaskStatus, on_delete=models.CASCADE)
+    enterprise = models.ForeignKey(Enterprise, on_delete=models.CASCADE)
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
